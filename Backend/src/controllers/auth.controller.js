029f994afb9d1e805084462789f0d7c26faa5c42
lostojicc <ostojiclukaftn@gmail.com>
2025-05-29 14:36:48 +0200

    Backend setup
    - setting up dependencies, project structure and auth

---
+++ Backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req, res) => {
+    res.send("signup route");
+};
+
+export const signin = (req, res) => {
+    res.send("signin route");
+};
+
+export const signout = (req, res) => {
+    res.send("signout route");
+};
