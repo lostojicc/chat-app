f4285a9e20cd9e6349fa25d5651bd0aa0aae83ed
lostojicc <ostojiclukaftn@gmail.com>
2025-05-30 18:09:34 +0200

    feat: Sign in, sign out and update profile
    - embedding jwt inside cookie for sign in
    - updating profile picture (using cloudinary for storing pictures)
    - added check authentication function for reloading the page

--- Backend/src/routes/auth.route.js
+++ Backend/src/routes/auth.route.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { signup, signin, signout } from "../controllers/auth.controller.js";
+import { signup, signin, signout, updateProfile, checkAuth } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
+
+router.get("/check", protectRoute, checkAuth);
 
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.post("/signout", signout);
+
+router.patch("/update-profile", protectRoute, updateProfile);
 
 export default router;
