a46d7f582ae6332cb1b964da58e0989c8ec38479
lostojicc <ostojiclukaftn@gmail.com>
2025-06-05 19:41:07 +0200

    test: Deployment bug test

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -25,10 +25,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../Frontend/dist")));
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"))
-    })
+  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
+  });
 }
 
 server.listen(PORT, () => {
